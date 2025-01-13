9226b061b84df15aba498a318b29e91bb99aecd3
Ibrahima Sarr <kognary@gmail.com>
2025-01-13 12:27:09 +0000

    reprise du blog

--- pages/users.tsx
+++
@@ -1,8 +0,0 @@
-import { sql } from '../db';
- 
-export const dynamic = 'force-dynamic';
- 
-export async function GET() {
-  await sql.query(`INSERT INTO users (name) VALUES ('John Doe');`);
-  return Response.json({ success: true });
-}
